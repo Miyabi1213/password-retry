c2beba898962bb30f49b27f99bef981da72f82bf
Miyabi <snow.miyabi1213@gmail.com>
2021-10-30 18:15:16 +0800

    a little change

--- password.py
+++ password.py
@@ -1,10 +1,14 @@
 password = 'a123456'
 test = 3
 while test != 0:
+    test = test - 1
     pwd = input('請輸入密碼：')
-    if pwd != password:
-        test = test - 1
-        print('密碼錯誤，還有', test, '次機會')
+    if pwd != password:    
+        print('密碼錯誤')
+        if test > 0:
+            print('還有', test, '次機會')
+        else:
+            print('沒機會嘗試了！')
     else:
-        print('登入成功')
+        print('登入成功！')
         break
